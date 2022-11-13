b430ecb72dd52ac2389ab080ea3fc2d38b7882f7
Dênnis Rodrigues <dennisrodrigues97@outlook.com>
2022-11-13 07:28:31 -0300

    Modificacao exercicio 27

--- Exer27.py
+++ Exer27.py
@@ -1,6 +1,21 @@
+#Exercício 27
 nome = str(input("Nome completo: ")).strip().split()
 primeiro = nome[0]
 ultimo = nome[len(nome)-1]
 
 print("Primeiro nome: {}\nUltimo nome: {}".format(primeiro, ultimo))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
