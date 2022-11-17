8b7a46d9efb4896236cb0ec31ff9cbc4e8958330
Dênnis Rodrigues <dennisrodrigues97@outlook.com>
2022-11-16 23:28:39 -0300

    Adicionado elif para caso não tenha Silva no nome.

--- Exer25B.py
+++ Exer25B.py
@@ -6,7 +6,15 @@ while i < len(nome):
    
     if nome[i] == "silva":
         print("Seu nome tem Silva!")
+    elif nome[i] != "silva " and i == len(nome)-1:
+        print("Você não tem Silva no nome!")
+
+
     i = i + 1
+
+
+
+
 
 
 
