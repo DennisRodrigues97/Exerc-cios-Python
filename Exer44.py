c953e7b9f857d60cdfe1e3dbb3cf6cfad0fc5519
Dênnis Rodrigues <dennisrodrigues97@outlook.com>
2022-11-23 15:00:26 -0300

    Adicionado opção invalida para caso seja escolhida uma opção que ão esteja presente.

--- Exer44.py
+++ Exer44.py
@@ -1,6 +1,6 @@
-print("Shampoo R$18,99")
+print("{:~^40}".format("Villares Móveis"))
 
-preco = 18.99
+preco = float(input("Valor das compras R$ "))
 a_vista = preco - (10 / 100) * preco
 a_vista_cartao = preco - (5 / 100) * preco
 parcelado3x = preco + (20 / 100) * preco
@@ -8,17 +8,26 @@ parcelado3x = preco + (20 / 100) * preco
 print("Qual a forma de pagamento?\n")
 tipo_pagamento = int(input("1 - DINHEIRO\n2 - CHEQUE\n3 - CARTÃO\n"))
 
-if tipo_pagamento == 1 or tipo_pagamento == 2:
+if tipo_pagamento != 1 and tipo_pagamento != 2 and tipo_pagamento != 3:
+    print("Opção inválida!")
+elif tipo_pagamento == 1 or tipo_pagamento == 2:
     preco = a_vista
+#    print("Valor a pagar R${:.2f}".format(preco))
 elif tipo_pagamento == 3:
     forma_pagamento = int(input("1 - À VISTA\n2 - PARCELADO\n"))
-    if forma_pagamento == 2:
+    if forma_pagamento != 1 or 2:
+        print("Opção inválida!")
+    elif forma_pagamento == 2:
         parcela = int(input("Número de parcelas: "))
         if parcela >= 3:
             preco = parcelado3x
         else:
             preco = a_vista_cartao
-    else:
+    elif forma_pagamento == 1:
         preco = a_vista_cartao
 
-print("Valor à pagar R${:.2f}".format(preco))
+
+
+
+
+
